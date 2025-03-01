d09636052609df7b5a600d4eb53184ddd6e70f95
Sauda001 <sauda.cse8.bu@gmail.com>
2025-03-01 08:51:51 +0600

    add money cash out complete

---
+++ script/addMoney.js
@@ -0,0 +1,21 @@
+// alert()
+document.getElementById('add-money').addEventListener('click', function(event){
+ event.preventDefault();
+ const amount = document.getElementById('amount').value;
+ const convertedAmount = parseFloat(amount);
+ const pin = document.getElementById('pin').value;
+ const convertedPin = parseInt(pin);
+ const mainBalance = document.getElementById('main-balance').innerText;
+ const convertedMainbalance = parseFloat(mainBalance);
+
+ if (amount && pin) {
+   if(convertedPin===1234){
+      const sum = convertedMainbalance+convertedAmount;
+      document.getElementById('main-balance').innerText = sum;
+   } else {
+      console.log('invalid pin');
+   }
+ } else {
+   alert('Null amount or Null pin')
+ }
+})
---
+++ script/cashout.js
@@ -0,0 +1,18 @@
+document.getElementById('cashout-btn').addEventListener('click', function(event){
+    event.preventDefault();
+    const pin = document.getElementById('cashout-pin').value;
+    const convertedPin = parseInt(pin);
+
+    const amount = document.getElementById('cashout-amount').value;
+    const convertedAmount = parseFloat(amount);
+
+    const mainBalance = document.getElementById('main-balance').innerText;
+    const convertedMainbalance = parseFloat(mainBalance);
+
+    if (convertedPin === 1234) {
+        const sub = convertedMainbalance - convertedAmount;
+        document.getElementById('main-balance').innerText = sub;
+    } else {
+        alert('invalid pin');
+    }
+})
